8d712841b4ce89f4546015691d64be98ac9e95c2
Bryan Dixon <javawolfpack@gmail.com>
2019-10-29 13:18:22 -0700

    Hello World MPI

---
+++ MPI/Python/helloworld.py
@@ -0,0 +1,15 @@
+"""
+Parallel Hello World
+https://github.com/erdc/mpi4py/blob/master/demo/helloworld.py
+"""
+
+from mpi4py import MPI
+import sys
+
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+name = MPI.Get_processor_name()
+
+sys.stdout.write(
+    "Hello, World! I am process %d of %d on %s.\n"
+    % (rank, size, name))
